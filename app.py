88ad863c7227d4e15d2508b8c1b4f615b4a1e8d6
Matheus Martins Lopes <martins6231@gmail.com>
2025-05-10 21:58:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,13 @@ st.set_page_config(
     layout="wide",
     page_icon="🧃",
 )
-
+col1, col2, col3 = st.columns([1,2,1])
+with col2:
+    st.image("britvic_logo.png", width=240)
+    st.markdown(
+        "<h1 style='text-align: center; color: #003057;'>🔎 Acompanhamento de Produção - Britvic</h1>", 
+        unsafe_allow_html=True
+    )
 # Adicionando a logo
 st.markdown(
     """
